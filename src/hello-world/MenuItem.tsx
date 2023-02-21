93859478bf8180de14d226e54b0aa5f38b6adfe6
palamar <ss380982599941@gmail.com>
2023-02-21 17:05:34 +0200

    added interface

--- src/hello-world/MenuItem.tsx
+++ src/hello-world/MenuItem.tsx
@@ -1,6 +1,5 @@
 import {useState} from "react";
 import {getInitialMenuObject, getInitialMenuUsersObject} from "./InitialState";
-import {IUser} from "../Interfaces/User";
 import {TableUsers} from "./TableUsers";
 import {Button} from "../Components/Button";
 import {Input} from "../Components/Input";
@@ -8,42 +7,12 @@ import {useUserMerge} from "../Hooks/useUser";
 import {IMenuItemWithUser} from "../Interfaces/MenuItemWithUser";
 import {IMenuItemWithUsersList} from "../Interfaces/MenuItemWithUsersList";
 
-type UserAction = (menuItem: string, users: IMenuItemWithUsersList) => IMenuItemWithUsersList;
-
 export function Item(props: {items: Array<{name: string}>}): JSX.Element {
   const {items} = props;
   const [menuItemWithUsers, setMenuItemUser] = useUserMerge<IMenuItemWithUser>(getInitialMenuObject);
-  const [users, setUsers] = useState<IMenuItemWithUsersList>(getInitialMenuUsersObject);
-  const [userAction, setUserAction] = useState<UserAction>(() => addUser);
+  const [users, setUsers] = useState<IMenuItemWithUsersList>(getInitialMenuUsersObject)
 
-  const addUser = (menuItem: string, users: IMenuItemWithUsersList): IMenuItemWithUsersList => {
-    const usersList: IMenuItemWithUsersList = {...users};
-
-    if (!usersList[menuItem]){
-      usersList[menuItem] = [];
-    }
-
-    usersList[menuItem].push({
-      name: menuItemWithUsers[menuItem].name,
-      surname: menuItemWithUsers[menuItem].surname,
-    });
-
-    setMenuItemUser({ [menuItem]: { name: '', surname: '' } });
-
-    return usersList;
-  };
-
-  const deleteLastUser = (menuItem: string, users: IMenuItemWithUsersList): IMenuItemWithUsersList => {
-    const usersList: IMenuItemWithUsersList = {...users};
-
-    usersList[menuItem].pop();
-
-    setMenuItemUser({ [menuItem]: { name: '', surname: '' } });
-
-    return usersList;
-  };
-
-  /*const addUser = (menuItem: string, users: IUser[]): IUser[] => setUsers((previousUsers: { [p: string]: IUser[] }) => {
+  const addUser = (event: any) => setUsers((previousUsers: IMenuItemWithUsersList) => {
     const {value: userItem} = event.target.dataset;
     const usersList = {...previousUsers};
 
@@ -59,7 +28,7 @@ export function Item(props: {items: Array<{name: string}>}): JSX.Element {
     setMenuItemUser({ [userItem]: { name: '', surname: '' } });
 
     return usersList;
-  });*/
+  });
 
   return (
     <>
@@ -67,7 +36,7 @@ export function Item(props: {items: Array<{name: string}>}): JSX.Element {
         items.map((item, index) => {
             return (<li key={index} className="item">
               <div style={{display: 'flex', flexDirection: 'column'}}>
-                <Button name={item.name} onClick={() => userAction}/>
+                <Button name={item.name} onClick={addUser}/>
                 <Input onChange={(name) => setMenuItemUser({ [item.name]: { name } })} value={menuItemWithUsers[item.name]?.name}/>
                 <Input onChange={(surname) => setMenuItemUser({ [item.name]: { surname } })} value={menuItemWithUsers[item.name]?.surname}/>
                 <TableUsers users={users[item.name]}/>
